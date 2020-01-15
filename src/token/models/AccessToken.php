15bfe5e353d60e577c5e9685e2f703b250dac896
Clay <clay@twopiz.com>
2020-01-16 01:42:09 +0800

    fix bug

--- src/token/models/AccessToken.php
+++ src/token/models/AccessToken.php
@@ -21,56 +21,66 @@ use yii\behaviors\TimestampBehavior;
 
 class AccessToken extends \yii\db\ActiveRecord
 {
-  public function behaviors()
-  {
-    return [
-      TimestampBehavior::className(),
-    ];
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public static function tableName()
-  {
-    return 'access_token';
-  }
+	public function behaviors()
+	{
+		return [
+			TimestampBehavior::className(),
+		];
+	}
 
-  /**
-   * {@inheritdoc}
-   */
-  public function rules()
-  {
-    return [
-      // [['user_id', 'token'], 'required'],
-      // [['user_id', 'status'], 'integer'],
-      // [['updated_at', 'created_at'], 'safe'],
-      [['created_at', 'updated_at'], 'date', 'format'=>'dd-MM-yyyy', 'message'=>'{attribute} must be DD/MM/YYYY format.'             ],
-      [['token'], 'string', 'max' => 255],
-      [['token'], 'unique'],
-      [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
-    ];
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function attributeLabels()
-  {
-    return [
-      'id' => Yii::t('app', 'ID'),
-      'user_id' => Yii::t('app', 'User ID'),
-      'status' => Yii::t('app', 'Status'),
-      'token' => Yii::t('app', 'Token'),
-      'updated_at' => Yii::t('app', 'Updated At'),
-      'created_at' => Yii::t('app', 'Created At'),
-    ];
-  }
+	public static function find()
+	{
+		if(!Yii::$app->user->identity->isAdmin) {
+			return parent::find()->where(['user_id' => Yii::$app->user->id]);
+		} 
+		return parent::find();
+	}
 
-  /**
-   * @return \yii\db\ActiveQuery
-   */
-  public function getUser()
-  {
-    return $this->hasOne(User::className(), ['id' => 'user_id']);
-  }
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'access_token';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			// [['user_id', 'token'], 'required'],
+			// [['user_id', 'status'], 'integer'],
+			// [['updated_at', 'created_at'], 'safe'],
+			[['created_at', 'updated_at'], 'date', 'format'=>'dd-MM-yyyy', 'message'=>'{attribute} must be DD/MM/YYYY format.'             ],
+			[['token'], 'string', 'max' => 255],
+			[['token'], 'unique'],
+			[['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => Yii::t('app', 'ID'),
+			'user_id' => Yii::t('app', 'User ID'),
+			'status' => Yii::t('app', 'Status'),
+			'token' => Yii::t('app', 'Token'),
+			'updated_at' => Yii::t('app', 'Updated At'),
+			'created_at' => Yii::t('app', 'Created At'),
+		];
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getUser()
+	{
+		return $this->hasOne(User::className(), ['id' => 'user_id']);
+	}
 }
--- src/token/models/AccessTokenSearch.php
+++ src/token/models/AccessTokenSearch.php
@@ -11,81 +11,81 @@ use claylua\token\models\AccessToken;
  */
 class AccessTokenSearch extends AccessToken
 {
-  /**
-   * {@inheritdoc}
-   */
-  public function rules()
-  {
-    return [
-      [['id', 'status'], 'integer'],
-      [['user_id', 'token', 'created_at', 'updated_at'], 'safe'],
-    ];
-  }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['id', 'status'], 'integer'],
+			[['user_id', 'token', 'created_at', 'updated_at'], 'safe'],
+		];
+	}
 
-  /**
-   * {@inheritdoc}
-   */
-  public function scenarios()
-  {
-    // bypass scenarios() implementation in the parent class
-    return Model::scenarios();
-  }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function scenarios()
+	{
+		// bypass scenarios() implementation in the parent class
+		return Model::scenarios();
+	}
 
-  /**
-   * Creates data provider instance with search query applied
-   *
-   * @param array $params
-   *
-   * @return ActiveDataProvider
-   */
-  public function search($params)
-  {
-    $query = AccessToken::find();
+	/**
+	 * Creates data provider instance with search query applied
+	 *
+	 * @param array $params
+	 *
+	 * @return ActiveDataProvider
+	 */
+	public function search($params)
+	{
+		$query = AccessToken::find();
 
-    $query->joinWith(['user']);
-    // add conditions that should always apply here
+		$query->joinWith(['user']);
+		// add conditions that should always apply here
 
-    $dataProvider = new ActiveDataProvider([
-      'query' => $query,
-    ]);
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-    // Important: here is how we set up the sorting
-    // The key is the attribute name on our "AccessTokenSearch" instance
-    $dataProvider->sort->attributes['email'] = [
-      // The tables are the ones our relation are configured to
-      // in my case they are prefixed with "tbl_"
-      'asc' => ['user.email' => SORT_ASC],
-      'desc' => ['user.email' => SORT_DESC],
-    ];
+		// Important: here is how we set up the sorting
+		// The key is the attribute name on our "AccessTokenSearch" instance
+		$dataProvider->sort->attributes['email'] = [
+			// The tables are the ones our relation are configured to
+			// in my case they are prefixed with "tbl_"
+			'asc' => ['user.email' => SORT_ASC],
+			'desc' => ['user.email' => SORT_DESC],
+		];
 
-    $this->load($params);
+		$this->load($params);
 
-    if (!$this->validate()) {
-      // uncomment the following line if you do not want to return any records when validation fails
-      // $query->where('0=1');
-      return $dataProvider;
-    }
+		if (!$this->validate()) {
+			// uncomment the following line if you do not want to return any records when validation fails
+			// $query->where('0=1');
+			return $dataProvider;
+		}
 
-    // grid filtering conditions
-    $query->andFilterWhere([
-      'access_token.id' => $this->id,
-      'status' => $this->status,
-    ])
-          ->andFilterWhere([
-            '>=',
-            'access_token.created_at',
-            strtotime($this->created_at)
-          ])
-          ->andFilterWhere([
-            '>=',
-            'access_token.updated_at',
-            strtotime($this->updated_at)
-          ])
-          ->andFilterWhere(['like', 'user.email', $this->user_id]);
+		// grid filtering conditions
+		$query->andFilterWhere([
+			'access_token.id' => $this->id,
+			'status' => $this->status,
+		])
+				->andFilterWhere([
+					'>=',
+					'access_token.created_at',
+					strtotime($this->created_at)
+				])
+				->andFilterWhere([
+					'>=',
+					'access_token.updated_at',
+					strtotime($this->updated_at)
+				])
+				->andFilterWhere(['like', 'user.email', $this->user_id]);
 
 
-    $query->andFilterWhere(['like', 'token', $this->token]);
+		$query->andFilterWhere(['like', 'token', $this->token]);
 
-    return $dataProvider;
-  }
+		return $dataProvider;
+	}
 }
